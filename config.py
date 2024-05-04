064d7d8945bb45dfb311ccb7f6d667ac8931536d
huriferenc <huri.ferenc@gmail.com>
2024-05-05 01:30:08 +0200

    Added sign data-frames of Person-2

--- config.py
+++ config.py
@@ -4,33 +4,54 @@ from pathlib import Path
 MIN_L = 2
 MAX_L = 10
 
-PERSON_ID = 1
+PERSON_ID = 2
 IMAGE_NUMBER = 24
 
-SIGN_FOLDER_ORIG = Path.cwd() / 'CEDAR' / 'full_org'
-SIGN_FOLDER_FORG = Path.cwd() / 'CEDAR' / 'full_forg'
+SIGN_FOLDER_ORIG = Path.cwd() / "CEDAR" / "full_org"
+SIGN_FOLDER_FORG = Path.cwd() / "CEDAR" / "full_forg"
 
-SAVING_FOLDER_ORIG = Path.cwd() / 'quasi_straight_line_segments' / f'person-{PERSON_ID}' / 'org'
-SAVING_FOLDER_FORG = Path.cwd() / 'quasi_straight_line_segments' / f'person-{PERSON_ID}' / 'forg'
+SAVING_FOLDER_ORIG = (
+    Path.cwd() / "quasi_straight_line_segments" / f"person-{PERSON_ID}" / "org"
+)
+SAVING_FOLDER_FORG = (
+    Path.cwd() / "quasi_straight_line_segments" / f"person-{PERSON_ID}" / "forg"
+)
 
-EXTRACTED_FEATURE_SET_SAVING_FOLDER = Path.cwd() / 'extracted_feature_set' / f'person-{PERSON_ID}'
+EXTRACTED_FEATURE_SET_SAVING_FOLDER = (
+    Path.cwd() / "extracted_feature_set" / f"person-{PERSON_ID}"
+)
 
-VISUALIZATION_FOLDER = Path.cwd() / 'visualizations' / f'person-{PERSON_ID}'
+VISUALIZATION_FOLDER = Path.cwd() / "visualizations" / f"person-{PERSON_ID}"
 
-CLASSIFIER_RESULTS_FOLDER = Path.cwd() / 'classifier_results' / f'person-{PERSON_ID}'
+CLASSIFIER_RESULTS_FOLDER = Path.cwd() / "classifier_results" / f"person-{PERSON_ID}"
 
-CLASSIFICATION_VISUALIZATION_FOLDER = Path.cwd() / 'classifier_visualizations' / f'person-{PERSON_ID}'
+CLASSIFICATION_VISUALIZATION_FOLDER = (
+    Path.cwd() / "classifier_visualizations" / f"person-{PERSON_ID}"
+)
 
-FNAME_ORIG = f'original_{PERSON_ID}' + '_{sign_index}.png'
-FNAME_FORG = f'forgeries_{PERSON_ID}' + '_{sign_index}.png'
+FNAME_ORIG = f"original_{PERSON_ID}" + "_{sign_index}.png"
+FNAME_FORG = f"forgeries_{PERSON_ID}" + "_{sign_index}.png"
 
-DATAFRAME_FNAME_ORIG = f'original_{PERSON_ID}' + '_{sign_index}.png.L_{l}.csv'
-DATAFRAME_FNAME_FORG = f'forgeries_{PERSON_ID}' + '_{sign_index}.png.L_{l}.csv'
+DATAFRAME_FNAME_ORIG = f"original_{PERSON_ID}" + "_{sign_index}.png.L_{l}.csv"
+DATAFRAME_FNAME_FORG = f"forgeries_{PERSON_ID}" + "_{sign_index}.png.L_{l}.csv"
 
-VISUALIZATION_FNAME_ORIG_ORIG = f'person_{PERSON_ID}' + '_orig_{sign_1}_orig_{sign_2}_{column}_L_{l}.png'
-VISUALIZATION_FNAME__ORIG_FORG = f'person_{PERSON_ID}' + '_orig_{sign_1}_forg_{sign_2}_{column}_L_{l}.png'
+VISUALIZATION_FNAME_ORIG_ORIG = (
+    f"person_{PERSON_ID}" + "_orig_{sign_1}_orig_{sign_2}_{column}_L_{l}.png"
+)
+VISUALIZATION_FNAME__ORIG_FORG = (
+    f"person_{PERSON_ID}" + "_orig_{sign_1}_forg_{sign_2}_{column}_L_{l}.png"
+)
 
-CLASSIFIER_RESULT_FNAME = f'result_person_{PERSON_ID}' + '_training_size_{training_size}x{training_size}_L_{l}_{time_index}.csv'
-CLASSIFIER_RESULT_FNAME_TEXT = f'result_person_{PERSON_ID}' + '_training_size_{training_size}x{training_size}_L_{l}_{time_index}.txt'
+CLASSIFIER_RESULT_FNAME = (
+    f"result_person_{PERSON_ID}"
+    + "_training_size_{training_size}x{training_size}_L_{l}_{time_index}.csv"
+)
+CLASSIFIER_RESULT_FNAME_TEXT = (
+    f"result_person_{PERSON_ID}"
+    + "_training_size_{training_size}x{training_size}_L_{l}_{time_index}.txt"
+)
 
-CLASSIFICATION_VISUALIZATION_FNAME = f'classification_person_{PERSON_ID}' + '_training_size_{training_size}x{training_size}_{classifier}_{time_index}.png'
+CLASSIFICATION_VISUALIZATION_FNAME = (
+    f"classification_person_{PERSON_ID}"
+    + "_training_size_{training_size}x{training_size}_{classifier}_{time_index}.png"
+)
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 from datetime import datetime
+
 import numpy as np
 import pandas as pd
 
+from config import (
+    EXTRACTED_FEATURE_SET_SAVING_FOLDER,
+    FNAME_FORG,
+    FNAME_ORIG,
+    IMAGE_NUMBER,
+    MAX_L,
+    MIN_L,
+    SAVING_FOLDER_FORG,
+    SAVING_FOLDER_ORIG,
+    SIGN_FOLDER_FORG,
+    SIGN_FOLDER_ORIG,
+)
 from edge_processor import EdgeProcessor
-
-from config import EXTRACTED_FEATURE_SET_SAVING_FOLDER, FNAME_FORG, FNAME_ORIG, IMAGE_NUMBER, MAX_L, MIN_L, SIGN_FOLDER_FORG, SIGN_FOLDER_ORIG, SAVING_FOLDER_FORG, SAVING_FOLDER_ORIG
-
 from params import C_NAMES, C_NUM, SEGMENT, START_SEGMENT
 
 
@@ -32,7 +42,7 @@ class App:
             "cp/P": [],
             "mCi": [],
             "rij/P": [],
-            "lRj": [[] for _ in range(C_NUM)]
+            "lRj": [[] for _ in range(C_NUM)],
         }
 
         max_Ci_in_regions = [
@@ -41,27 +51,31 @@ class App:
             {"Ci": 0, "count": 0},
             {"Ci": 0, "count": 0},
             {"Ci": 0, "count": 0},
-            {"Ci": 0, "count": 0}
+            {"Ci": 0, "count": 0},
         ]
 
         for i, item in enumerate(class_segments):
             DQSSi, ni, pi, DQSSi_img = item
-            
+
             # Ha nincs adott osztályú szegmens, akkor folytassa a következő osztállyal
             if ni == 0:
-                data_table['ni'].append(0)
-                data_table['pi/P'].append(0)
-                data_table['pi/ni'].append(0)
-                data_table['cp/P'].append(0)
-                data_table['mCi'].append(0)
-                data_table['rij/P'].append(0)
+                data_table["ni"].append(0)
+                data_table["pi/P"].append(0)
+                data_table["pi/ni"].append(0)
+                data_table["cp/P"].append(0)
+                data_table["mCi"].append(0)
+                data_table["rij/P"].append(0)
                 continue
 
             # cp - Közös pixelek száma a szomszédos osztállyal
             # Ci and Cj, j = (i+1) mod 12
-            _, _, _, DQSSi_neighbour_img = class_segments[(i+1) % C_NUM]
-            same_pixel_condition = np.logical_and(np.logical_or(DQSSi_img == START_SEGMENT, DQSSi_img == SEGMENT), np.logical_or(
-                DQSSi_neighbour_img == START_SEGMENT, DQSSi_neighbour_img == SEGMENT))
+            _, _, _, DQSSi_neighbour_img = class_segments[(i + 1) % C_NUM]
+            same_pixel_condition = np.logical_and(
+                np.logical_or(DQSSi_img == START_SEGMENT, DQSSi_img == SEGMENT),
+                np.logical_or(
+                    DQSSi_neighbour_img == START_SEGMENT, DQSSi_neighbour_img == SEGMENT
+                ),
+            )
             cp = np.sum(same_pixel_condition)
 
             # mCi - Annak a régiónak az azonosítója, ahol a legtöbb Ci oszályú pixel létezik
@@ -71,20 +85,21 @@ class App:
             part_width = width // 3
             regions = [
                 DQSSi_img[0:part_height, 0:part_width],
-                DQSSi_img[0:part_height, part_width:2*part_width],
-                DQSSi_img[0:part_height, 2*part_width:3*part_width],
-                DQSSi_img[part_height:2*part_height, 0:part_width],
-                DQSSi_img[part_height:2 *
-                          part_height, part_width:2*part_width],
-                DQSSi_img[part_height:2 *
-                          part_height, 2*part_width:3*part_width]
+                DQSSi_img[0:part_height, part_width : 2 * part_width],
+                DQSSi_img[0:part_height, 2 * part_width : 3 * part_width],
+                DQSSi_img[part_height : 2 * part_height, 0:part_width],
+                DQSSi_img[part_height : 2 * part_height, part_width : 2 * part_width],
+                DQSSi_img[
+                    part_height : 2 * part_height, 2 * part_width : 3 * part_width
+                ],
             ]
 
             mCi = 0
             rij = 0
             for index, region in enumerate(regions):
-                pixels_count = np.sum(np.logical_or(
-                    region == START_SEGMENT, region == SEGMENT))
+                pixels_count = np.sum(
+                    np.logical_or(region == START_SEGMENT, region == SEGMENT)
+                )
 
                 if pixels_count > rij:
                     rij = pixels_count
@@ -94,15 +109,15 @@ class App:
                     max_Ci_in_regions[index]["count"] = pixels_count
                     max_Ci_in_regions[index]["Ci"] = i
 
-            data_table['ni'].append(ni)
-            data_table['pi/P'].append(pi/P)
-            data_table['pi/ni'].append(pi/ni)
-            data_table['cp/P'].append(cp/P)
-            data_table['mCi'].append(mCi+1)
-            data_table['rij/P'].append(rij/P)
+            data_table["ni"].append(ni)
+            data_table["pi/P"].append(pi / P)
+            data_table["pi/ni"].append(pi / ni)
+            data_table["cp/P"].append(cp / P)
+            data_table["mCi"].append(mCi + 1)
+            data_table["rij/P"].append(rij / P)
 
         for region_index, max_Ci in enumerate(max_Ci_in_regions):
-            data_table['lRj'][max_Ci["Ci"]].append(region_index + 1)
+            data_table["lRj"][max_Ci["Ci"]].append(region_index + 1)
 
         return pd.DataFrame(data_table)
 
@@ -122,21 +137,29 @@ class App:
         efs_saving_folder = EXTRACTED_FEATURE_SET_SAVING_FOLDER
 
         for sign_id in range(IMAGE_NUMBER):
-            print(f'Sign number: {sign_id + 1}')
+            print(f"Sign number: {sign_id + 1}")
 
             filename_original = FNAME_ORIG.format(sign_index=sign_id + 1)
             filename_forgery = FNAME_FORG.format(sign_index=sign_id + 1)
 
             for l in range(MIN_L, MAX_L + 1):
-                print(f'Threshold: {l}')
+                print(f"Threshold: {l}")
 
-                class_segments_original, P_original = self.process_edges(l, filename_original, SIGN_FOLDER_ORIG, saving_folder_original)
+                class_segments_original, P_original = self.process_edges(
+                    l, filename_original, SIGN_FOLDER_ORIG, saving_folder_original
+                )
                 df_original = self.getDataFrame(class_segments_original, P_original)
-                self.export_feature_set(f'{filename_original}.L_{l}.csv', efs_saving_folder, df_original)
+                self.export_feature_set(
+                    f"{filename_original}.L_{l}.csv", efs_saving_folder, df_original
+                )
 
-                class_segments_forgery, P_forgery = self.process_edges(l, filename_forgery, SIGN_FOLDER_FORG, saving_folder_forgery)
+                class_segments_forgery, P_forgery = self.process_edges(
+                    l, filename_forgery, SIGN_FOLDER_FORG, saving_folder_forgery
+                )
                 df_forgery = self.getDataFrame(class_segments_forgery, P_forgery)
-                self.export_feature_set(f'{filename_forgery}.L_{l}.csv', efs_saving_folder, df_forgery)
+                self.export_feature_set(
+                    f"{filename_forgery}.L_{l}.csv", efs_saving_folder, df_forgery
+                )
 
         end_time = datetime.now()
         print(f"Ending time: {end_time}")
